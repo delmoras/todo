7703cb65a17fc68e9215960ef565c8ace2ab1aba
jm <jm@dtek.gr>
2022-12-13 17:47:37 +0200

    finished tutorial

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlusSquare,faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faPlusSquare,faTrash)
-Vue.component('font-awesome-icon',FontAwesomeIcon)
 
-createApp(App).mount("#app")
+
+library.add(faPlusSquare,faTrash)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
+
+
+
--- resources/js/vue/listView.vue
+++ resources/js/vue/listView.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        Form
+    <div class="items">
+        <div v-for="(item,index) in items" :key="index">
+            <list-item
+                :item="item"
+                class="item"
+                v-on:itemchanged="$emit('reloadlist')"
+            />
+        </div>
     </div>
 </template>
+
+<script>
+
+import listItem from "./listItem.vue"
+
+
+export default{
+    props: ['items'],
+
+    components:{
+        listItem
+    }
+}
+</script>
+<style scoped>
+.item{
+    background:#e6e6e6;
+    padding:5px;
+    margin-top:5px;
+}
+</style>
